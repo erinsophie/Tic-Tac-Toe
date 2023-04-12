7029493ac3320253364d3b2c409fd62acf8203be
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-04-12 19:54:39 +0100

    Add data index

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// ARRAY WITH POSSIBLE WINNING COMBOS
+
+const winningCombos = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [0, 4, 8],
+  ];
