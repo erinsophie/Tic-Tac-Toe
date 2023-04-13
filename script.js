db4ce02880c20da4f826698482eaeb3e2c57cdc1
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-04-13 14:18:52 +0100

    Module for board status, updating and resetting board

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // PLAYER FACTORY FUNCTION 
-
 const Player = (marker) => {
   const getMarker = () => marker;
   return { getMarker };
 };
+
+
+// MODULE FOR GAME BOARD 
+const GameBoard = (() => {
+  let board = ['', '', '', '', '', '', '', '', ''];
+
+  const getBoard = () => board;
+
+  const updateBoard = (index, value) => {
+    board[index] = value;
+  };
+
+  const resetBoard = () => {
+    board = ['', '', '', '', '', '', '', '', ''];
+  };
+
+  return { getBoard, updateBoard, resetBoard };
+})();
 
 
 
