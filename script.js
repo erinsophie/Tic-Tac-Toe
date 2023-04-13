96356f0fa49d69d8de5b8b5681bf2f9436208762
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-04-13 14:17:44 +0100

    Player factory function

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+// PLAYER FACTORY FUNCTION 
+
+const Player = (marker) => {
+  const getMarker = () => marker;
+  return { getMarker };
+};
 
 
 
 
-// ARRAY WITH POSSIBLE WINNING COMBOS
 
-const winningCombos = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-    [1, 4, 7],
-    [2, 4, 6],
-    [2, 5, 8],
-    [0, 4, 8],
-  ];
+
+
+
+
